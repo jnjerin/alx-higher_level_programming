ef2c775763ce104d49244fe25c4434b89d68935a
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-15 19:45:28 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,7 +3,9 @@
 -- If show doesn't have genre, display NULL
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres
-  LEFT JOIN tv_shows 
-  ON tv_show_genres.show_id = tv_shows.id
-  ORDER BY tv_shows.title, tv_show_genres.genre_id;
+	FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
